5a2ccd017a7b7a8cb0425d12fb959691e080e1ac
Yury Habets <over17@outlook.com>
2016-05-06 12:16:51 +0200

    Apply non-fullscreen mode before the splash screen. Add UI visibility listener that forces non-fullscreen mode

--- Assets/Plugins/Android/UnityShowAndroidStatusBar/src/com/unity3d/nostatusbar/UnityPlayerActivityStatusBar.java
+++ Assets/Plugins/Android/UnityShowAndroidStatusBar/src/com/unity3d/nostatusbar/UnityPlayerActivityStatusBar.java
@@ -1,6 +1,8 @@
 package com.unity3d.nostatusbar;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
 import com.unity3d.player.UnityPlayerActivity;
 
@@ -11,5 +13,48 @@ public class UnityPlayerActivityStatusBar extends UnityPlayerActivity
 	{
 		super.onCreate(savedInstanceState);
 		getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		// Clear low profile flags to apply non-fullscreen mode before splash screen
+		showSystemUi();
+		addUiVisibilityChangeListener();
+	}
+
+	private static int getLowProfileFlag()
+	{
+		return Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT
+		?
+			View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY |
+			View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
+			View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
+			View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
+			View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
+			View.SYSTEM_UI_FLAG_FULLSCREEN
+		:
+			View.SYSTEM_UI_FLAG_LOW_PROFILE;
+	}
+
+	private void showSystemUi()
+	{
+		// Works from API level 11
+		if (android.os.Build.VERSION.SDK_INT < android.os.Build.VERSION_CODES.HONEYCOMB)
+			return;
+
+		mUnityPlayer.setSystemUiVisibility(mUnityPlayer.getSystemUiVisibility() & ~getLowProfileFlag());
+	}
+	
+	private void addUiVisibilityChangeListener()
+	{
+		// Works from API level 11
+		if (android.os.Build.VERSION.SDK_INT < android.os.Build.VERSION_CODES.HONEYCOMB)
+			return;
+
+		mUnityPlayer.setOnSystemUiVisibilityChangeListener(new View.OnSystemUiVisibilityChangeListener()
+		{
+			@Override
+			public void onSystemUiVisibilityChange(final int visibility)
+			{
+				// Whatever changes - force status/nav bar to be visible
+				showSystemUi();
+			}
+		});
 	}
 }
